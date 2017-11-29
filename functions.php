d54d017d2a16e85f43095992c55408b5a6749419
Daniel Franco <32742876+danielrfranco@users.noreply.github.com>
2017-11-29 09:49:07 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_theme_support( 'title-tag' );
 add_theme_support( 'post-thumbnails' );
 
 function register_main_menu() {
-  register_nav_menu('main-menu', 'Header Menu');
+  register_nav_menu('main-menu', 'Main Menu');
 }
 add_action( 'init', 'register_main_menu' );
 ?>
